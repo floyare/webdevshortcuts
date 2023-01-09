f2154a0fcedf575f0f3a283759d9dea7d3b2db2f
floyare <neptuneeex@gmail.com>
2023-01-09 17:59:08 +0100

    Changed websites list to local file

--- src/Components/Category.jsx
+++ src/Components/Category.jsx
@@ -1,25 +1,19 @@
 import { useHistory, useParams } from "react-router-dom";
 import WebsiteItem from "./WebsiteItem";
 import { Icon } from '@iconify/react';
-import websites from '../Websites.json';
+import websitesStatic from '../Websites.json';
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Category = () => {
   const {name} = useParams();
   const history = useHistory();
-  const [categories, setCategories] = useState(null);
+  const [websites, setWebsites] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     setIsLoading(true);
-    axios.get('https://api.jsonbin.io/v3/b/63bc0fda15ab31599e31dbca', {headers: {
-      'X-Master-Key': import.meta.env.VITE_JSON_MASTER_KEY
-    }}).then((res) => {
-      console.warn(res)
-      const result = res.data.record.filter(x => x.category === name);
-      setCategories(result);
-      setIsLoading(false);
-    })
+    setWebsites(websitesStatic.filter(x => x.category === name));
+    setIsLoading(false);
   },[])
 
   return ( 
@@ -29,8 +23,8 @@ const Category = () => {
         {isLoading && <div className="loading__indicator">
           <div className="cube"><div></div><div></div><div></div><div></div><div></div><div></div></div>
         </div>}
-        {(categories && categories.length === 0) && <h4>No websites in this category :(</h4>}
-        {categories && categories.map((web) => {
+        {(websites && websites.length === 0) && <h4>No websites in this category :(</h4>}
+        {websites && websites.map((web) => {
           return(
             <WebsiteItem data={web} key={web.id}/>
           );
